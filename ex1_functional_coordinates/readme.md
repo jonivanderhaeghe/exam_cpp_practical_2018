ac06ed1594375e285db6a46abd401224f70f9e9e
Nico De Witte <nico.dewitte@vives.be>
2018-06-14 14:58:20 +0200

    Add assignment on functional programming conversion

---
+++ ex1_functional_coordinates/readme.md
@@ -0,0 +1,25 @@
+# Functional Coordinates
+
+Deze applicatie is functioneel geschreven en heeft een serieuze refactor nodig. Refactor de app zodat er op een object georienteerde manier wordt gewerkt. Herwerk ook de volledige main.
+
+Zorg dat de applicatie is opgebouwd volgens de regels van de kunst.
+
+De applicatie is volledig compileerbaar in zijn startende staat.
+
+Vergeet niet van `tasks.json` aan te passen om je nieuwe classes ook mee te compileren.
+
+## De verwachte output
+
+De applicatie zou op het einde van de rit de volgende output moeten genereren (zelfde als in de startende staat).
+
+```text
+Start: (12.340000, 32.880000)
+End: (-15.470000, 11.770000)
+
+Distance: 34.914584
+Mid of the two points: (-1.565000, 22.325000)
+```
+
+## Remarks
+
+<!-- Hier kan je opmerkingen plaatsen. -->
---
+++ ex1_functional_coordinates/src/main.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+double determine_distance(double x1, double y1, double x2, double y2) {
+    return sqrt((x2 - x1)*(x2 - x1) + (y2 - y1)*(y2 - y1));
+}
+
+void print_to_screen(double x, double y) {
+    cout << "(" << to_string(x) << ", " << to_string(y) << ")" << endl;
+}
+
+void get_the_midpoint(double x1, double y1, double x2, double y2, double * xmid, double * ymid) {
+    // Result is returned in the pointers xmid and ymid as we can only return a single value
+
+    *xmid = (x1 + x2) / 2;
+    *ymid = (y1 + y2) / 2;
+}
+
+int main(void) {
+
+    double x1 = 0;
+    double x2 = 0;
+    double y1 = 0;
+    double y2 = 0;
+
+    x1 = 12.34;
+    y1 = 32.88;
+    
+    x2 = -15.47;
+    y2 = 11.77;
+
+    // Print start and end
+    cout << "Start: ";
+    print_to_screen(x1, y1);
+    cout << "End: ";
+    print_to_screen(x2, y2);
+    cout << endl;
+
+    cout << "Distance: " << to_string(determine_distance(x1, y1, x2, y2)) << endl;
+
+    double xmid;
+    double ymid;
+    get_the_midpoint(x1, y1, x2, y2, &xmid, &ymid);
+    cout << "Mid of the two points: ";
+    print_to_screen(xmid, ymid);
+    cout << endl;
+
+    
+
+    return 0;
+}
